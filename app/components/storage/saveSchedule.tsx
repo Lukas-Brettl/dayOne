5601ea9332a2fa75a3ee8e196e968d0c94c1d152
brettl <lukas.brettl@seznam.cz>
2025-07-30 16:18:50 +0200

    finally add to schedule when change frequency by close button

---
+++ app/components/storage/saveSchedule.tsx
@@ -0,0 +1,35 @@
+import Load from "./loadFromStorage";
+import SaveToStorage from "./saveToStorage";
+
+export default async function Schedule(days:string[], category:string, nameHabit:string):Promise<void>{
+    
+    const loadedHabitsData = await Load('habits')
+   const loadedScheduleData = await Load('schedule') as Record<string, Record<string, { XP: number, time?: string }>>
+    const habit = loadedHabitsData[category][nameHabit]
+    
+    let dataToSave = {...loadedScheduleData}
+    
+    if (!days || days.length === 0) {
+    
+        for(const [day, singlehabit] of Object.entries(dataToSave)){
+            if(singlehabit[nameHabit]){
+                delete singlehabit[nameHabit]
+            }
+        }
+    }
+    days.forEach(day => {
+        if(!dataToSave[day]){
+            dataToSave[day] = {}
+        }
+        dataToSave[day][nameHabit] = {
+            'time': habit['time'],
+            'XP': habit['XP']
+        }
+    })
+
+    
+    SaveToStorage({
+        items:dataToSave,
+        keyName:'schedule'
+        })
+}
